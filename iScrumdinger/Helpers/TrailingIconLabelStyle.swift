6293a64faa6b234d2bab871be3084a87441e589e
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-07-25 23:04:18 +0300

    Display a list of daily scrums, Make scrums identifiable,

---
+++ iScrumdinger/Helpers/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  iScrumdinger
+//
+//  Created by jopootrivatel on 25.07.2023.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+    
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
