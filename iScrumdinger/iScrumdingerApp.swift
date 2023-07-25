0f90a839238409ca9a32fe1ba1a23e472bd7188c
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-07-25 21:40:21 +0300

    Initial Commit

---
+++ iScrumdinger/iScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iScrumdingerApp.swift
+//  iScrumdinger
+//
+//  Created by jopootrivatel on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
