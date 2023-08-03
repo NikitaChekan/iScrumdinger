d98e2b614d1c450278b242fdaf2556a1f9e60a97
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-08-03 22:23:21 +0300

    Add scrum history,

--- iScrumdinger/Models/History.swift
+++ iScrumdinger/Models/History.swift
@@ -9,7 +9,14 @@ import Foundation
 import AVFoundation
 
 struct History: Identifiable {
+    
     let id: UUID
     let date: Date
     var attendees: [DailyScrum.Attendee]
+    
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
 }
