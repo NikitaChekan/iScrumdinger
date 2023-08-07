af8bde54373a7201dfeeeeaada80f52f44181f25
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-08-07 19:59:38 +0300

    Create the meeting timer view, Draw an arc segment, Draw the progress ring

--- iScrumdinger/Views/MeetingView.swift
+++ iScrumdinger/Views/MeetingView.swift
@@ -25,8 +25,9 @@ struct MeetingView: View {
                     secondRemaining: scrumTimer.secondsRemaining,
                     theme: scrum.theme
                 )
-                Circle()
-                    .strokeBorder(lineWidth: 24)
+                
+                MeetingTimerView(speakers: scrumTimer.speakers, theme: scrum.theme)
+                
                 MeetingFooterView(
                     speakers: scrumTimer.speakers,
                     skipAction: scrumTimer.skipSpeaker
